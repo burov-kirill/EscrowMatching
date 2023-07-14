d8fdfcb495196ca44d9ec274e660a784b617148e
Буров Кирилл <k.burov@SD.LOCAL>
2023-07-14 15:45:48 +0300

    check

--- settings/update/config.py
+++ settings/update/config.py
@@ -1,6 +1,6 @@
 APP_URL = 'https://raw.githubusercontent.com/burov-kirill/EscrowMatching/master/dist/EscrowMatching.exe'
 ZIP_URL = ''
 APP_NAME = 'EscrowMatching.exe'
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 VERSION_URL = 'https://raw.githubusercontent.com/burov-kirill/EscrowMatching/main/__VERSION__'
 UPDATE_URL = 'https://raw.githubusercontent.com/burov-kirill/Updater/master/dist/update.exe'
